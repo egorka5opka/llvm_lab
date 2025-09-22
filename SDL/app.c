8b316f6a0e39d679e047aeecb062fe48d1c93166
Egor Tuisov <egortuisov@gmail.com>
2025-09-22 04:41:45 +0300

    Brain's brains simulation

---
+++ SDL/app.c
@@ -0,0 +1,79 @@
+#include "sim.h"
+
+#define CELL_SIZE 4
+#define ROWS (SIM_X_SIZE / CELL_SIZE)
+#define COLS (SIM_Y_SIZE / CELL_SIZE)
+#define COLOR_DEAD 0xff000000
+#define COLOR_DYING 0xff38ffeb 
+#define COLOR_ALIVE 0xffffff
+#define DEAD 0
+#define DYING 1
+#define ALIVE 2
+
+int data[2][ROWS][COLS];
+
+
+void draw_cell(int row, int col, int color) {
+  for (int i = 0; i < CELL_SIZE; ++i) {
+    for (int j = 0; j < CELL_SIZE; ++j) {
+      simPutPixel(row * CELL_SIZE + i, col * CELL_SIZE + j, color);
+    }
+  }
+}
+
+
+void app() {
+
+  for (int i = ROWS / 2 - 4; i < ROWS / 2 + 4; ++i) {
+    for (int j = ROWS / 2 - 4; j < ROWS / 2 + 4; ++j) {
+        if (((i * j) >> 3) & 1) {
+          data[0][i][j] = ALIVE;
+        }
+    }
+  }
+
+  int step = 0;
+  while (1) {
+
+    for (int r = 0; r < ROWS; ++r) {
+      for (int c = 0;c < COLS; ++c) {
+        int cur = data[step][r][c];
+        int color = DEAD;
+        if (cur == ALIVE) {
+          color = COLOR_ALIVE;
+        } else if (cur == DYING) {
+          color = COLOR_DYING;
+        }
+        draw_cell(r, c, color);
+      }
+    }
+    simFlush();
+
+    int next_step = 1 - step;
+    for (int r = 0; r < ROWS; ++r) {
+      for (int c = 0;c < COLS; ++c) {
+        int cur = data[step][r][c];
+        int nxt = DEAD;
+        if (cur == ALIVE) {
+          nxt = DYING;
+        } else if (cur == DYING) {
+          nxt = DEAD;
+        } else {
+          int cnt = 0;
+          for (int dx = -1; dx <= 1; ++dx) {
+            for (int dy = -1; dy <= 1; ++dy) {
+              if (data[step][(r + dx + ROWS) % ROWS][(c + dy + COLS) % COLS] == ALIVE) {
+                ++cnt;
+              }
+            }
+          }
+          if (cnt == 2) {
+            nxt = ALIVE;
+          }
+        }
+        data[next_step][r][c] = nxt;
+      }
+    }
+    step = next_step;
+  }
+}
---
+++ SDL/sim.h
@@ -0,0 +1,11 @@
+#define SIM_X_SIZE 512
+#define SIM_Y_SIZE 512
+
+#ifndef __sim__
+void simInit();
+void app();
+void simExit();
+void simFlush();
+void simPutPixel(int x, int y, int argb);
+int simRand();
+#endif
